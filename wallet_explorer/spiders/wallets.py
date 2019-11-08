40123e80e75968fe1631f5f10cbabc2f0bdfc58f
Diogo Canut <diogocanut@hotmail.com>
2019-11-07 23:14:51 -0300

    fix: callback args

--- wallet_explorer/spiders/wallets.py
+++ wallet_explorer/spiders/wallets.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from functools import partial
 
 
 class WalletsSpider(scrapy.Spider):
@@ -7,25 +8,23 @@ class WalletsSpider(scrapy.Spider):
     allowed_domains = ['walletexplorer.com']
     start_urls = ['http://walletexplorer.com/']
     global wallets
-    gwallet = ''
 
     def parse(self, response):
         r = response.xpath('//table/tr/td/ul/li/a/@href').extract()[:30]
         self.wallets = r
         for wallet in r:
-            for page in range(0,20):
+            for page in range(0,5):
                 next_page = response.urljoin(str(wallet) + '?page={0}'.format(page))
-                self.gwallet = wallet
-                yield scrapy.Request(next_page, callback=self.parse_wallets)
+                yield scrapy.Request(next_page, callback=self.parse_wallets, cb_kwargs={'wallet': wallet})
 
-    def parse_wallets(self, response):
+    def parse_wallets(self, response, wallet):
         page_transactions = response.xpath('//table/tr')
         # page_transactions = response.xpath('//table/tr/td/a/@href').extract()
         for full_transaction in page_transactions:
             transaction = full_transaction.xpath('td/a/@href').extract_first()
             if transaction in self.wallets:
                 tx = transaction.split('/')[-1]
-                wl = self.gwallet.split('/')[-1]
+                wl = wallet.split('/')[-1]
                 amount = full_transaction.css('.amount::text').get().replace('\xa0','')
                 print(tx + "  ---  " + wl + "  --- " + amount)
                 yield {'wallet1': tx, 'wallet2': wl, 'amount': amount}
